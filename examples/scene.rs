f9d2af2eab8901c7476d407bd8bedf3797a62447
mattatz <masatatsu.nakamura@gmail.com>
2024-12-16 22:08:37 +0900

    Upgrade to 0.7.0 for bevy 0.15.

--- examples/scene.rs
+++ examples/scene.rs
@@ -1,8 +1,9 @@
 use bevy::{
     math::primitives::Cuboid,
+    pbr::MeshMaterial3d,
     prelude::{
-        App, Assets, Camera3dBundle, ClearColor, Color, Commands, MaterialMeshBundle, Mesh, ResMut,
-        Startup, Transform, Vec3,
+        App, Assets, Camera3d, ClearColor, Color, Commands, Mesh, Mesh3d, ResMut, Startup,
+        Transform, Vec3,
     },
     DefaultPlugins,
 };
@@ -22,15 +23,14 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<NormalMaterial>>,
 ) {
-    commands.spawn(MaterialMeshBundle {
-        mesh: meshes.add(Mesh::from(Cuboid::default())),
-        transform: Transform::from_xyz(0.0, 0.25, 0.0),
-        material: materials.add(NormalMaterial::default()),
-        ..Default::default()
-    });
+    commands.spawn((
+        Mesh3d(meshes.add(Mesh::from(Cuboid::default()))),
+        Transform::from_xyz(0.0, 0.25, 0.0),
+        MeshMaterial3d(materials.add(NormalMaterial::default())),
+    ));
 
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
